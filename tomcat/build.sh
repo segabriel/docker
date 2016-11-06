d44b516eba6be1d8d112f74f097ebd50cba67589
segabriel <sergiy.gabriel@gmail.com>
2016-11-06 20:12:43 +0200

    Added tomcat scripts

---
+++ tomcat/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+IMAGE_NAME="segabriel/tomcat:8.5.6"
+
+docker rmi -f ${IMAGE_NAME}
+docker build -t ${IMAGE_NAME} .
---
+++ tomcat/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CONTAINER_NAME="tomcat"
+IMAGE_NAME="segabriel/tomcat:8.5.6"
+TOMCAT_WEBAPPS="/opt/tomcat/webapps"
+
+mkdir -p ${TOMCAT_WEBAPPS}
+chmod -R 777 ${TOMCAT_WEBAPPS}
+
+docker stop ${CONTAINER_NAME}
+docker rm ${CONTAINER_NAME}
+
+docker run \
+	--detach \
+	--publish 8090:8080 \
+	--name ${CONTAINER_NAME} \
+	${IMAGE_NAME}
+
+docker inspect ${CONTAINER_NAME}
+docker logs -f ${CONTAINER_NAME}

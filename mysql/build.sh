6862cf0eb35483bc044794236e0fc1ee421022bb
segabriel <sergiy.gabriel@gmail.com>
2016-11-13 22:56:12 +0200

    Improved mysql scripts

--- mysql/build.sh
+++ mysql/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 MYSQL_IMAGE_NAME="segabriel/mysql:5.7"
 
-docker rmi -f ${MYSQL_IMAGE_NAME}
+#docker rmi -f ${MYSQL_IMAGE_NAME}
 docker build -t ${MYSQL_IMAGE_NAME} .
--- mysql/run.sh
+++ mysql/run.sh
@@ -5,26 +5,36 @@ MYSQL_USER="development"
 MYSQL_PASSWORD="userSecretPassword"
 MYSQL_DATABASE="dataBaseName"
 
+MYSQL_REMOTE_ROOT_NAME="remote"
+MYSQL_REMOTE_ROOT_PASS="remoteSecretPassword"
+
 MYSQL_CONTAINER_NAME="mysql"
 MYSQL_IMAGE_NAME="segabriel/mysql:5.7"
-MYSQL_DATASTORAGE="/opt/mysql/data"
+#MYSQL_DATASTORAGE="/opt/mysql/data"
 
-mkdir -p ${MYSQL_DATASTORAGE}
-chmod -R 777 ${MYSQL_DATASTORAGE}
+#rm -rf ${MYSQL_DATASTORAGE}
+#mkdir -p ${MYSQL_DATASTORAGE}
+#chmod -R 777 ${MYSQL_DATASTORAGE}
 
 docker stop ${MYSQL_CONTAINER_NAME}
 docker rm ${MYSQL_CONTAINER_NAME}
 
 docker run \
 	--detach \
+	--env TERM=xterm \
 	--env MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
 	--env MYSQL_USER=${MYSQL_USER} \
 	--env MYSQL_PASSWORD=${MYSQL_PASSWORD} \
 	--env MYSQL_DATABASE=${MYSQL_DATABASE} \
-	--volume ${MYSQL_DATASTORAGE}:/var/lib/mysql \
+	--env MYSQL_REMOTE_ROOT_NAME=${MYSQL_REMOTE_ROOT_NAME} \
+	--env MYSQL_REMOTE_ROOT_PASS=${MYSQL_REMOTE_ROOT_PASS} \
 	--publish 3306:3306 \
 	--name ${MYSQL_CONTAINER_NAME} \
 	${MYSQL_IMAGE_NAME}
 
 docker inspect ${MYSQL_CONTAINER_NAME}
 docker logs -f ${MYSQL_CONTAINER_NAME}
+
+#	--volume ${MYSQL_DATASTORAGE}:/var/lib/mysql \
+
+#docker run -it --rm -p 3306:3306 --env MYSQL_ROOT_PASSWORD=root segabriel/mysql:5.7 /bin/bash

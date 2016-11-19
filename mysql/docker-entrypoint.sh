b1109d310366a40e59edb5bdbd3d0314f1aa8c6a
segabriel <sergiy.gabriel@gmail.com>
2016-11-19 14:21:46 +0200

    Improved initializing mysql

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -16,16 +16,22 @@ MYSQL_REMOTE_ROOT_HOST=${MYSQL_REMOTE_ROOT_HOST:-"172.17.0.1"}
 if [[ ! -d ${MYSQL_VOLUME_HOME}/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $MYSQL_VOLUME_HOME"
 
+	# The default password for the debian-sys-maint user is randomly generated
+	# during the installation of the mysql-server package.
+	# Due to the nature of docker we blank out the password such that the maintenance
+	# user can login without a password.
+	sed 's/password = .*/password = /g' -i /etc/mysql/debian.cnf
+
     if [ -z ${MYSQL_ROOT_PASSWORD} ]; then
-		echo >&2 'error: database is uninitialized and password option is not specified '
-		echo >&2 '=> You need to specify MYSQL_ROOT_PASSWORD '
+		echo >&2 "=> error: database is uninitialized and password option is not specified "
+		echo >&2 "=> You need to specify MYSQL_ROOT_PASSWORD "
 		exit 1
 	fi
 
 	mkdir -p ${MYSQL_VOLUME_HOME}
 	chown -R mysql:mysql ${MYSQL_VOLUME_HOME}
 
-	echo '=> Initializing database'
+	echo "=> Initializing database"
 
     # Try the 'preferred' solution
     mysqld --initialize-insecure > /dev/null 2>&1
@@ -41,21 +47,22 @@ if [[ ! -d ${MYSQL_VOLUME_HOME}/mysql ]]; then
 
     mysql=( mysql --protocol=socket -uroot -hlocalhost)
 
+	echo -n "=> Waiting MySQL init process in progress"
     for i in {30..0}; do
-		if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+		if echo "SELECT 1" | "${mysql[@]}" &> /dev/null; then
 			break
 		fi
-		echo '=> MySQL init process in progress...'
+		echo -n "."
 		sleep 1
 	done
+	echo
 	if [ "$i" = 0 ]; then
-		echo >&2 '=> MySQL init process failed.'
+		echo >&2 "=> MySQL init process failed."
 		exit 1
 	fi
 
+	echo "=> Drop all users and creating 'root'@'localhost' user..."
 	"${mysql[@]}" <<-EOSQL
-		-- What's done in this file shouldn't be replicated
-		--  or products like mysql-fabric won't work
 		SET @@SESSION.SQL_LOG_BIN=0;
 		DELETE FROM mysql.user ;
 		DROP USER root@localhost ;
@@ -65,18 +72,19 @@ if [[ ! -d ${MYSQL_VOLUME_HOME}/mysql ]]; then
 		FLUSH PRIVILEGES ;
 	EOSQL
 
-	if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
-		mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
-	fi
+	mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
 
-    if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-		echo >&2 'error: database is uninitialized and password option is not specified '
-		echo >&2 '=> You need to specify MYSQL_ROOT_PASSWORD '
-		exit 1
-	fi
+	# The debian-sys-maint is used while creating users and database
+	# as well as to shut down or starting up the mysql server via mysqladmin
+	echo "=> Creating 'debian-sys-maint'@'localhost' user..."
+	"${mysql[@]}" <<-EOSQL
+		CREATE USER 'debian-sys-maint'@'localhost' IDENTIFIED BY '' ;
+		GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;
+		FLUSH PRIVILEGES ;
+	EOSQL
 
 	if [ -n "${MYSQL_REMOTE_ROOT_NAME}" -a -n "${MYSQL_REMOTE_ROOT_HOST}" ]; then
-		echo "=> Creating remote user \"${MYSQL_REMOTE_ROOT_NAME}\" with root privileges..."
+		echo "=> Creating remote '$MYSQL_REMOTE_ROOT_NAME'@'$MYSQL_REMOTE_ROOT_HOST' user with root privileges..."
 		"${mysql[@]}" <<-EOSQL
 			CREATE USER '${MYSQL_REMOTE_ROOT_NAME}'@'${MYSQL_REMOTE_ROOT_HOST}' IDENTIFIED BY '${MYSQL_REMOTE_ROOT_PASS}' ;
 			GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_REMOTE_ROOT_NAME}'@'${MYSQL_REMOTE_ROOT_HOST}' WITH GRANT OPTION ;
@@ -85,20 +93,21 @@ if [[ ! -d ${MYSQL_VOLUME_HOME}/mysql ]]; then
 	fi
 
 	if [ "$MYSQL_DATABASE" ]; then
-		echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
+		echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" | "${mysql[@]}"
 		mysql+=( "$MYSQL_DATABASE" )
 	fi
 
 	if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+		echo "=> Creating '$MYSQL_USER'@'%' user for '$MYSQL_DATABASE' ..."
 		echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
 		if [ "$MYSQL_DATABASE" ]; then
 			echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
 		fi
-		echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+		echo "FLUSH PRIVILEGES ;" | "${mysql[@]}"
 	fi
 
     echo
-	echo '=> MySQL init process done. Ready for start up.'
+	echo "=> MySQL init process done. Ready for start up."
 	echo
 
 	mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown

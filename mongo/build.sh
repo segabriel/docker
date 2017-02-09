a32c2a2321605188837d040a40058989b0f6412e
segabriel <sergiy.gabriel@gmail.com>
2017-02-09 21:36:39 +0200

    Updated mongo version

--- mongo/build.sh
+++ mongo/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IMAGE_NAME="segabriel/mongo:3.3.15"
+IMAGE_NAME="segabriel/mongo:3.4.2"
 
 #docker rmi -f ${IMAGE_NAME}
 docker build -t ${IMAGE_NAME} .
--- mongo/run.sh
+++ mongo/run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 CONTAINER_NAME="mongo"
-IMAGE_NAME="segabriel/mongo:3.3.15"
-MONGO_VOLUME_DATA_STORAGE="/opt/mongo/data/db"
-MONGO_VOLUME_DATA_CONFIG="/opt/mongo/data/configdb"
+IMAGE_NAME="segabriel/mongo:3.4.2"
+#MONGO_VOLUME_DATA_STORAGE="/opt/mongo/data/db"
+#MONGO_VOLUME_DATA_CONFIG="/opt/mongo/data/configdb"
 
-mkdir -p ${MONGO_VOLUME_DATA_STORAGE} ${MONGO_VOLUME_DATA_CONFIG}
-chmod -R 777 ${MONGO_VOLUME_DATA_STORAGE} ${MONGO_VOLUME_DATA_CONFIG}
+#mkdir -p ${MONGO_VOLUME_DATA_STORAGE} ${MONGO_VOLUME_DATA_CONFIG}
+#chmod -R 777 ${MONGO_VOLUME_DATA_STORAGE} ${MONGO_VOLUME_DATA_CONFIG}
 
 docker stop ${CONTAINER_NAME}
 docker rm ${CONTAINER_NAME}
 
+#docker run \
+#	--detach \
+#	--volume ${MONGO_VOLUME_DATA_STORAGE}:/data/db \
+#	--volume ${MONGO_VOLUME_DATA_CONFIG}:/data/configdb \
+#	--publish 27017:27017 \
+#	--name ${CONTAINER_NAME} \
+#	${IMAGE_NAME}
+
 docker run \
 	--detach \
-	--volume ${MONGO_VOLUME_DATA_STORAGE}:/data/db \
-	--volume ${MONGO_VOLUME_DATA_CONFIG}:/data/configdb \
 	--publish 27017:27017 \
 	--name ${CONTAINER_NAME} \
 	${IMAGE_NAME}
